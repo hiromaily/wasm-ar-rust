9c7243fc4fd307164dae087bffc6e129b5f74454
HirokiYasui <hiromaily2@gmail.com>
2024-09-22 14:10:55 +0900

    refctoring frontend

---
+++ wasm-frontend/src/lib/components/Video.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+import * as wasm from "image-wasm";
+import { onMount } from "svelte";
+import { saveOriginalImage, saveOutputImage } from "../images";
+
+let video: HTMLVideoElement;
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+let initialized = false;
+
+// initialization
+const setup = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    await video.play();
+    initialized = true;
+  } catch (error) {
+    console.error("Failed to setup video stream:", error);
+  }
+};
+
+// process each frame
+const processFrame = () => {
+  try {
+    if (!initialized || !context) return;
+
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    // call wasm function
+    const rgbaBuffer = wasm.process_image(
+      new Uint8Array(imageData.data.buffer),
+      canvas.width,
+      canvas.height,
+    );
+
+    const outputImageData = new ImageData(
+      new Uint8ClampedArray(rgbaBuffer),
+      canvas.width,
+      canvas.height,
+    );
+    // reflect images on canvas context
+    context.putImageData(outputImageData, 0, 0);
+
+    requestAnimationFrame(processFrame);
+  } catch (error) {
+    console.error("Error processing frame:", error);
+  }
+};
+
+// full screen
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+};
+
+onMount(async () => {
+  try {
+    // initialize
+    await setup();
+
+    canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    canvas.style.position = "absolute";
+    canvas.style.top = "0";
+    canvas.style.left = "0";
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context = canvas.getContext("2d")!;
+
+    // button event
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        toggleFullScreen();
+      } else if (event.key === "s" || event.key === "S") {
+        console.log("S: saveOutputImage");
+        saveOutputImage(context, canvas);
+      } else if (event.key === "o" || event.key === "O") {
+        console.log("O: saveOriginalImage");
+        saveOriginalImage(context, video);
+      }
+    });
+
+    processFrame();
+  } catch (error) {
+    console.error("Error during onMount:", error);
+  }
+});
+</script>
+
+<video bind:this={video} autoplay playsinline></video>
---
+++ wasm-frontend/src/lib/images/index.ts
@@ -0,0 +1,64 @@
+import * as wasm from "image-wasm";
+
+// Save image from canvas
+const saveImage = (tempCanvas: HTMLCanvasElement, filename: string) => {
+  const dataURL = tempCanvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = dataURL;
+  link.download = filename;
+  link.click();
+};
+
+// save original camera image
+export const saveOriginalImage = (
+  context?: CanvasRenderingContext2D,
+  video?: HTMLVideoElement,
+) => {
+  if (!context || !video) return;
+
+  // Create a temporary canvas to capture the original video frame
+  const tempCanvas = document.createElement("canvas");
+  tempCanvas.width = video.videoWidth;
+  tempCanvas.height = video.videoHeight;
+  const tempContext = tempCanvas.getContext("2d");
+  tempContext?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+  saveImage(tempCanvas, "original_image.png");
+};
+
+// save output image with wasm function
+export const saveOutputImage = (
+  context?: CanvasRenderingContext2D,
+  canvas?: HTMLCanvasElement,
+) => {
+  if (!context || !canvas) return;
+
+  // Create a temporary canvas to draw the current frame
+  const tempCanvas = document.createElement("canvas");
+  tempCanvas.width = canvas.width;
+  tempCanvas.height = canvas.height;
+  const tempContext = tempCanvas.getContext("2d");
+
+  // Draw the current processed frame onto the temporary canvas
+  tempContext?.drawImage(canvas, 0, 0);
+
+  saveImage(tempCanvas, "edge_detected_image.png");
+};
+
+// save output image with wasm function
+export const saveOutputImageWithWasm = (
+  context?: CanvasRenderingContext2D,
+  canvas?: HTMLCanvasElement,
+) => {
+  if (!context || !canvas) return;
+
+  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  const rgbaBuffer = new Uint8Array(imageData.data.buffer);
+  // call wasm function
+  const base64String = wasm.save_image(rgbaBuffer, canvas.width, canvas.height);
+
+  const link = document.createElement("a");
+  link.href = `data:image/png;base64,${base64String}`;
+  link.download = "edge_detected_image.png";
+  link.click();
+};
