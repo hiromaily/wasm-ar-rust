ddb056997cf39aab67cc328ed0a17688772c1135
HirokiYasui <hiromaily2@gmail.com>
2024-10-06 15:33:57 +0900

    add alert when gpu is disabled

--- wasm-frontend/src/App.svelte
+++ wasm-frontend/src/App.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 import DetectionCamera from "./lib/components/DetectionCamera3.svelte";
+import NoWebGPU from "./lib/components/NoWebGPU.svelte";
 // import Video from "./lib/components/EffectVideo.svelte";
 // import WasmCanvas from "./lib/components/WasmCanvas.svelte";
 
-const activeComponent = "detection"; // video, detection, canvas
+if (!navigator.gpu) {
+  alert(
+    "If you are using iPhone, WebGPU in WebKit Feature Flags must be enabled to use this application by Safari.",
+  );
+}
 </script>
 
 <main>
-  <DetectionCamera />
+  <!-- <DetectionCamera /> -->
+
+  {#if navigator.gpu}
+    <DetectionCamera />
+  {:else}
+    <NoWebGPU />
+  {/if}
+
   <!-- {#if activeComponent == "video"}
     <Video />
   {:else if activeComponent == "detection"}
---
+++ wasm-frontend/src/lib/components/NoWebGPU.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+const images = [
+  "./webgpu-featureflag.png",
+  "./webgpu-featureflag2.png",
+  "./webgpu-featureflag3.png",
+];
+</script>
+
+<div class="image-container">
+  {#each images as imageUrl}
+    <img src={imageUrl} alt="webgpu with webkit" />
+  {/each}
+</div>
+
+<style>
+  .image-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-y: auto;
+    height: 100vh;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+  }
+</style>
